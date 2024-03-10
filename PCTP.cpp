aa65d13ad0158bba24162db39a58553dcc24f31d
Dx-by-Dy <alsmirnov2004@gmail.com>
2024-03-10 18:38:20 +0300

    cpp

---
+++ PCTP.cpp
@@ -0,0 +1,82 @@
+﻿/*
+Parameter change testing program
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <chrono>
+#include <conio.h>
+
+#define sleep(x) std::this_thread::sleep_for(std::chrono::milliseconds(x))
+
+using namespace std;
+
+int main()
+{
+
+    FILE* fl = fopen("C:\\Users\\user\\Desktop\\test.txt", "a+t");
+
+    if (fl == NULL) {
+        std::cout << "Ошибка открытия файла";
+        exit(1);
+    }
+    char a;
+
+    while (true) {
+        //fflush(fl);
+        //fputs("test\n", fl);
+        //fflush(fl);
+
+        if (!feof(fl)) {
+            /*
+            char line[100];
+            while (fgets(line, 100, fl)) {
+                printf("%s", line);
+            }
+            */
+
+            char line[100];
+            //fscanf(fl, "%s", line);
+
+            
+            while (fgets(line, sizeof(line), fl)) {
+
+                cout << line;
+
+                //fseek(fl, 0, SEEK_CUR - 1);
+                //fseek(fl, 0, SEEK_CUR + 2);
+            }
+
+            fl = fopen("C:\\Users\\user\\Desktop\\test.txt", "w+");
+            
+            //fseek(fl, 0, SEEK_SET);
+            //fgets(line, sizeof(line), fl);
+            //cout << line;
+            //int d = atoi(line);
+
+            //for (auto chr : line) {
+            //    cout << (wchar_t)chr;
+            //}
+
+            //fseek(fl, 0, SEEK_SET);
+            //fputs("t", fl);
+
+            //std::cout << line << endl;
+            //fputs(" ", fl);
+        }
+
+        sleep(300);
+
+        if (_kbhit())
+        {
+            char a = _getch();
+            if (a == 'q')
+                return 0;
+            std::cout << a + 1 << std::endl;
+        }
+
+    }
+
+    fclose(fl);
+}
---
+++ test_of_test.cpp
@@ -0,0 +1,44 @@
+﻿/*
+Parameter change testing program
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+#include <chrono>
+#include <conio.h>
+
+#define sleep(x) std::this_thread::sleep_for(std::chrono::milliseconds(x))
+
+using namespace std;
+
+int main()
+{
+
+    FILE* fl = fopen("C:\\Users\\user\\Desktop\\test.txt", "r+");
+
+    if (fl == NULL) {
+        std::cout << "Ошибка открытия файла";
+        exit(1);
+    }
+    char a;
+
+    while (true) {
+        fseek(fl, 0, 0);
+        fputs("testing\n", fl);
+        fflush(fl);
+
+        sleep(300);
+
+        if (_kbhit())
+        {
+            char a = _getch();
+            if (a == 'q')
+                return 0;
+            std::cout << a + 1 << std::endl;
+        }
+
+    }
+
+    fclose(fl);
+}
